4173c4768f93aa88bea919ad7f324de084b060d9
Abetranda <106234528+Abetranda@users.noreply.github.com>
2023-06-09 18:01:41 +0700

    Add files via upload

---
+++ paceshop/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>TM SHOP & FASHION SPORT | SKH</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div class="header">
+    <p> TM SHOP & FASHION SPORT| SKH</p>
+  </div>
+  <nav>
+    <ul> <?php include("page/navbar.php") ?> </ul>
+  </nav>
+
+  <?php include("content.php") ?>
+
+  <?php include("page/user/keranjang.php") ?>
+  <div class="footer">
+    
+  </div>
+
+</body>
+
+</html>
